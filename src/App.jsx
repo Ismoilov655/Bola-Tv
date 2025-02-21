a22ca4f7866abdafc5c032dda6da565b5051029e
Ismoilov655 <ismoilov.m.zohid@gmail.com>
2025-02-21 18:05:17 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,29 @@ import Ertaklar from './pages/Ertaklar'
 import Multfilm from './pages/Multfilm'
 import Qoshiqlar from './pages/Qoshiqlar'
 import Murojaat from './pages/Murojaat'
+import QiziqarliMatematika from './pages/QiziqarliMatematika'
+import InglizTili from './pages/InglizTili'
+import Badantarbiya from './pages/Badantarbiya'
+import Rasm from './pages/Rasm'
 
 const App = () => {
   const routes = createBrowserRouter(
     createRoutesFromElements(
-      <Route element={<MainLayout/>}>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/ertaklar' element={<Ertaklar/>}/>
-        <Route path='/multfilm' element={<Multfilm/>}/>
-        <Route path="/qo'shiqlar" element={<Qoshiqlar/>}/>
-        <Route path='/qiziqarli matematika' element={<Home/>}/>
-        <Route path='/ingliz tili' element={<Home/>}/>
-        <Route path='/badantarbiya' element={<Home/>}/>
-        <Route path='/rasm' element={<Home/>}/>
-        <Route path='/murojaat' element={<Murojaat/>}/>
+      <Route element={<MainLayout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/ertaklar' element={<Ertaklar />} />
+        <Route path='/multfilm' element={<Multfilm />} />
+        <Route path="/qo'shiqlar" element={<Qoshiqlar />} />
+        <Route path='/qiziqarli matematika' element={<QiziqarliMatematika />} />
+        <Route path='/ingliz tili' element={<InglizTili/>} />
+        <Route path='/badantarbiya' element={<Badantarbiya/>} />
+        <Route path='/rasm' element={<Rasm/>} />
+        <Route path='/murojaat' element={<Murojaat />} />
       </Route>
     )
   )
   return (
-      <RouterProvider router={routes}/>
+    <RouterProvider router={routes} />
   )
 }
 
